78cbb9d75fca89a8ec5124eb9d23525d4618c17d
anusbaig <anusbaig08@gmail.com>
2023-05-27 04:45:52 +0500

    Add basic app layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { Grid, GridItem, Hide, Show } from "@chakra-ui/react";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        sm: `"nav" "main"`,
+        md: `"nav nav" "slider main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
+      <GridItem area='nav' bg='teal'>
+        Nav
+      </GridItem>
+      <Show breakpoint='(min-width: 768px) and (max-width: 992px)'>
+        <GridItem area='slider' bg='pink'>
+          Slider
+        </GridItem>
+      </Show>
+      <Show above='lg'>
+        <GridItem area='aside' bg='coral'>
+          Aside
+        </GridItem>
+      </Show>
+      <GridItem area='main' bg='gold'>
+        Main
+      </GridItem>
+    </Grid>
+  );
 }
 
-export default App
+export default App;
