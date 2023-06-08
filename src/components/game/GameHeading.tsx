351c4947fb5c8102cd0c63d7642d4f11bc08b866
anusbaig <anusbaig08@gmail.com>
2023-06-09 02:23:01 +0500

    Create lookup hooks for platform and genre

--- src/components/game/GameHeading.tsx
+++ src/components/game/GameHeading.tsx
@@ -1,21 +1,15 @@
 import { Heading } from "@chakra-ui/react";
-import React from "react";
+import useGenre from "../../hooks/useGenre";
+import usePlatform from "../../hooks/usePlatform";
 import GameQuery from "../../models/queries/gameQuery";
-import useGeneres from "../../hooks/useGenres";
-import usePlatforms from "../../hooks/usePlatforms";
 
 interface Props {
   gameQuery: GameQuery;
 }
 
 const GameHeading = ({ gameQuery }: Props) => {
-  const { data: genres } = useGeneres();
-  const genre = genres?.find((genre) => genre.id === gameQuery.genreId);
-
-  const { data: platforms } = usePlatforms();
-  const platform = platforms?.find(
-    (platform) => platform.id === gameQuery.platformId
-  );
+  const { data: genre } = useGenre(gameQuery.genreId);
+  const { data: platform } = usePlatform(gameQuery.platformId);
 
   const heading = `${platform?.name || ""} ${genre?.name || ""} Games`;
   return (
--- src/components/game/PlatformSelector.tsx
+++ src/components/game/PlatformSelector.tsx
@@ -2,6 +2,7 @@ import { Button, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
 import { BsChevronDown } from "react-icons/bs";
 import Platform from "../../models/platform";
 import usePlatforms from "../../hooks/usePlatforms";
+import usePlatform from "../../hooks/usePlatform";
 
 interface Props {
   selectedPlatformId?: number;
@@ -10,9 +11,7 @@ interface Props {
 
 const PlatformSelector = ({ selectedPlatformId, onSelectPlatform }: Props) => {
   const { data, error } = usePlatforms();
-  const selectedPlatform = data?.find(
-    (platform) => platform.id === selectedPlatformId
-  );
+  const { data: selectedPlatform } = usePlatform(selectedPlatformId);
 
   if (error) {
     console.log(error);
