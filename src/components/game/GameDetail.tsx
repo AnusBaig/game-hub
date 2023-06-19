ebb626cdfacef6501b79583cb189a64fce4fb197
anusbaig <anusbaig08@gmail.com>
2023-06-19 23:20:01 +0500

    Add expandable game description

--- src/components/game/GameDetail.tsx
+++ src/components/game/GameDetail.tsx
@@ -1,6 +1,7 @@
 import { Box, Heading, Text } from "@chakra-ui/react";
 import { useParams } from "react-router-dom";
 import useGameDetail from "../../hooks/useGameDetail";
+import ExpandableText from "../utils/ExpandableText";
 import Loader from "../utils/Loader";
 
 const GameDetail = () => {
@@ -10,14 +11,14 @@ const GameDetail = () => {
 
   if (isLoading) return <Loader />;
 
-  if (error || !game) return <p>Unable to fetch Game deatils</p>;
+  if (error || !game) return <Text>Unable to fetch Game deatils</Text>;
 
   return (
     <Box margin={5}>
       <Heading mb={5} fontSize='5xl'>
         {game.name_original}
       </Heading>
-      <Text fontSize='lg'>{game.description_raw}</Text>
+      <ExpandableText text={game.description_raw} />
     </Box>
   );
 };
